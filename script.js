c30d934c3e08316e710fcc3c640ba5ef2584ffd9
cedricced-net <nikilnik65@gmail.com>
2024-12-04 21:46:25 +0300

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const buttonsCollection= document.getElementsByClassName('btn-perform');
 for (let i = 0; i < buttonsCollection.length; i++){
     let button = buttonsCollection[i];
-    button.addEventListener('click', function (event){
+    button.addEventListener('click', function toggleClass(){
         const searchClass = event.target.closest('li');
         const targetClass = searchClass.target.closest('span');
         if (targetClass){
