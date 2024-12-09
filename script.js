5e476dd39d50f5ebe8569a3ed9900b5386ea9312
cedricced-net <nikilnik65@gmail.com>
2024-12-09 23:43:31 +0300

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// Копка Вып
+// Копка Вып (test task)
 const buttonsCollection= document.getElementsByClassName('btn-perform');
 for (let i = 0; i < buttonsCollection.length; i++){
     let button = buttonsCollection[i];
-    button.addEventListener('click', toggleClass)
+    button.addEventListener('click', toggleClass);
         function toggleClass(event){
             const parentEl = event.target.closest('li');
             const targetSpan = parentEl.querySelector('span');
@@ -11,11 +11,11 @@ for (let i = 0; i < buttonsCollection.length; i++){
             }
         }
 }
-//Кнопка del
+//Кнопка del (test task)
 const buttonsCollectionDel= document.getElementsByClassName('btn-del');
 for (let i = 0; i < buttonsCollectionDel.length; i++){
     let button = buttonsCollectionDel[i];
-    button.addEventListener('click', delTask)
+    button.addEventListener('click', delTask);
         function delTask(event){
             const delTaskIn = event.target.closest('li')
             if (delTask){
@@ -23,11 +23,49 @@ for (let i = 0; i < buttonsCollectionDel.length; i++){
             }
         }
 }
-//Кнопка добавления задачи
-const form= document.getElementById('add-task-form');
-const button= document.getElementById('btn-add');
-button.addEventListener('click', function(event){
-    event.preventDefault()
-    alert('временно присвоил айди кнопке')
-})
+// Добавление задачи + кнопки
+const form = document.getElementById('add-task-form');
+const button = document.getElementById('btn-add');
+const taskList = document.querySelector('.task-list');
 
+button.addEventListener('click', (event) => {
+event.preventDefault();
+
+const newTaskInput = document.getElementById('new-task');
+const taskText = newTaskInput.value;
+
+const li = document.createElement('li');
+const span = document.createElement('span');
+span.textContent = taskText;
+li.appendChild(span);
+
+const delButton = document.createElement('button');
+delButton.innerText = 'Удалить';
+li.appendChild(delButton);
+
+const performButton = document.createElement('button');
+performButton.innerText = 'Вып';
+li.appendChild(performButton);
+
+delButton.addEventListener('click', delTask);
+function delTask(event) {
+    const taskItem = event.target.closest('li'); 
+    if (taskItem) {
+    taskItem.remove();
+    }
+}
+
+performButton.addEventListener('click', toggleClass);
+function toggleClass(event) {
+    const taskItem = event.target.closest('li'); 
+    const targetSpan = taskItem.querySelector('span');
+    if (targetSpan) {
+    targetSpan.classList.toggle('done');
+    }
+}
+
+taskList.appendChild(li);
+
+newTaskInput.value = ''; 
+
+})
