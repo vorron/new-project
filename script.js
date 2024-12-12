be71a61f379755f29c2392b43785b097f670d9bf
cedricced-net <nikilnik65@gmail.com>
2024-12-12 22:57:52 +0300

    script.js

--- script.js
+++ script.js
@@ -1,71 +1,44 @@
-// Копка Вып (test task)
-const buttonsCollection= document.getElementsByClassName('btn-perform');
-for (let i = 0; i < buttonsCollection.length; i++){
-    let button = buttonsCollection[i];
-    button.addEventListener('click', toggleClass);
-        function toggleClass(event){
-            const parentEl = event.target.closest('li');
-            const targetSpan = parentEl.querySelector('span');
-            if (targetSpan){
-                targetSpan.classList.toggle('done');
-            }
-        }
-}
-//Кнопка del (test task)
-const buttonsCollectionDel= document.getElementsByClassName('btn-del');
-for (let i = 0; i < buttonsCollectionDel.length; i++){
-    let button = buttonsCollectionDel[i];
-    button.addEventListener('click', delTask);
-        function delTask(event){
-            const delTaskIn = event.target.closest('li')
-            if (delTask){
-                delTaskIn.remove()
-            }
-        }
-}
-// Добавление задачи + кнопки
-const form = document.getElementById('add-task-form');
-const button = document.getElementById('btn-add');
-const taskList = document.querySelector('.task-list');
+//(тестовые задачи)
+const testRenderTaskList= document.getElementById('task-list')
+const data=[
+    {label:'Изучить JS'},
+    {label:'Почистить клавиатуру'},
+    {label:'Купить батарейки'},
+];
 
-button.addEventListener('click', (event) => {
-event.preventDefault();
+for (const item of data){
+    const li= document.createElement('li');
+    const span= document.createElement('span');
+        span.textContent= `${item.label}`;
+    li.appendChild(span);
+    testRenderTaskList.appendChild(li);
+    
+    const testDelButton = document.createElement('button');
+    testDelButton.innerText = 'Удалить';
+    li.appendChild(testDelButton);
+    testDelButton.addEventListener('click',delTestTask);
+    
+    const testPerformButton = document.createElement('button');
+    testPerformButton.innerText = 'Вып';
+    li.appendChild(testPerformButton);
+    testPerformButton.addEventListener('click', testToggleDone);
+};
 
-const newTaskInput = document.getElementById('new-task');
-const taskText = newTaskInput.value;
-
-const li = document.createElement('li');
-const span = document.createElement('span');
-span.textContent = taskText;
-li.appendChild(span);
-
-const delButton = document.createElement('button');
-delButton.innerText = 'Удалить';
-li.appendChild(delButton);
-
-const performButton = document.createElement('button');
-performButton.innerText = 'Вып';
-li.appendChild(performButton);
-
-delButton.addEventListener('click', delTask);
-function delTask(event) {
+function delTestTask(event) {
     const taskItem = event.target.closest('li'); 
     if (taskItem) {
     taskItem.remove();
     }
-}
-
-performButton.addEventListener('click', toggleClass);
-function toggleClass(event) {
+};
+function testToggleDone(event) {
     const taskItem = event.target.closest('li'); 
     const targetSpan = taskItem.querySelector('span');
     if (targetSpan) {
     targetSpan.classList.toggle('done');
     }
-}
+};
+//(/тестовые задачи)
+//(основыне функции)
 
-taskList.appendChild(li);
 
-newTaskInput.value = ''; 
-
-})
+//(/основные функции)
